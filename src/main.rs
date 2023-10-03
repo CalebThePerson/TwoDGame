3e0646f4739e0895271f88859ba37bafd8f035c1
Caleb <Calebtheeperson@gmail.com>
2023-10-02 19:32:09 -0700

    pluh

--- src/main.rs
+++ src/main.rs
@@ -12,34 +12,83 @@ struct TestGame {
 #[async_trait::async_trait]
 impl Game for TestGame {
     async fn init(&mut self, engine: &mut Engine) {
+        //Creating our background image texture, by calling load texture
+        let (img, mut imgpng) = engine
+            .load_texture(
+                "/Users/calebtheperson/RustProjects/somuchpain/scene2d/src/assets/background.jpg",
+                None,
+            )
+            .await
+            .expect("Couldn't load background");
+
+        //Then we are adding this and behind the scenes it shoudl be creating a bind group and etc to display it.
+        engine.sprites.add_sprite_group(
+            &engine.gpu,
+            &img,
+            vec![GPUSprite {
+                screen_region: [0.0, 0.0, 1024.0, 768.0],
+                sheet_region: [0.0, 0.0, 1.0, 1.0],
+            }],
+            self.camera,
+        );
+
+        //Same thing but for our sprite
         let (tex_king, mut kingtex_kingpng) = engine
             .load_texture(
                 "/Users/calebtheperson/RustProjects/somuchpain/scene2d/src/king.png",
                 None,
             )
             .await
-            .expect("Couldn't load 47 img");
+            .expect("Couldn't load king img");
 
+        //This sprite-group we would want to add obstacles and etc
         engine.sprites.add_sprite_group(
             &engine.gpu,
-            tex_king,
+            &tex_king,
+            vec![
+                GPUSprite {
+                    screen_region: [192.0, 192.0, 64.0, 64.0],
+                    sheet_region: [0.0, 0.0, 0.5, 0.5], //[0.0, 0.5, 0.5, 0.5]
+                },
+                GPUSprite {
+                    screen_region: [192.0 + 64.0, 192.0, 64.0, 64.0],
+                    sheet_region: [0.0, 0.0, 0.5, 0.5], //[0.0, 0.5, 0.5, 0.5]
+                },
+                GPUSprite {
+                    screen_region: [192.0 + 128.0, 192.0, 64.0, 64.0],
+                    sheet_region: [0.0, 0.0, 0.5, 0.5], //[0.0, 0.5, 0.5, 0.5]
+                },
+                GPUSprite {
+                    screen_region: [192.0 + 192.0, 192.0, 64.0, 64.0],
+                    sheet_region: [0.0, 0.0, 0.5, 0.5], //[0.0, 0.5, 0.5, 0.5]
+                },
+            ],
+            self.camera,
+        );
+
+        //This sprite group adds the left Player
+        engine.sprites.add_sprite_group(
+            &engine.gpu,
+            &tex_king,
             vec![
                 //It's the 2 different sprites for king.png at 2 different locations
                 GPUSprite {
-                    screen_region: [32.0, 32.0, 64.0, 64.0],
+                    screen_region: [32.0, 96.0, 64.0, 64.0],
                     sheet_region: [0.0, 16.0 / 32.0, 16.0 / 32.0, 16.0 / 32.0],
                 },
+            ],
+            self.camera,
+        );
+
+        //This sprite group adds the right player
+        engine.sprites.add_sprite_group(
+            &engine.gpu,
+            &tex_king,
+            vec![
+                //It's the 2 different sprites for king.png at 2 different locations
                 GPUSprite {
-                    screen_region: [32.0, 128.0, 64.0, 64.0],
-                    sheet_region: [16.0 / 32.0, 16.0 / 32.0, 16.0 / 32.0, 16.0 / 32.0],
-                },
-                GPUSprite {
-                    screen_region: [128.0, 32.0, 64.0, 64.0],
+                    screen_region: [384.0, 96.0, 64.0, 64.0],
                     sheet_region: [0.0, 16.0 / 32.0, 16.0 / 32.0, 16.0 / 32.0],
-                },
-                GPUSprite {
-                    screen_region: [128.0, 128.0, 64.0, 64.0],
-                    sheet_region: [16.0 / 32.0, 16.0 / 32.0, 16.0 / 32.0, 16.0 / 32.0],
                 },
             ],
             self.camera,
@@ -56,9 +105,6 @@ impl Game for TestGame {
 fn main() {
     let camera = GPUCamera {
         screen_pos: [0.0, 0.0],
-        // Consider using config.width and config.height instead,
-        // it's up to you whether you want the window size to change what's visible in the game
-        // or scale it up and down
         screen_size: [1024.0, 768.0],
     };
     let event_loop = winit::event_loop::EventLoop::new();
